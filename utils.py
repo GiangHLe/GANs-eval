6fd94e702ffc59df028e63de6bb47753a0e775a9
KR95334 <kr95334@navercorp.com>
2021-10-12 23:17:19 +0900

    move the adjust dynamic range to FID class to use it dynamic in training process

--- utils.py
+++ utils.py
@@ -5,17 +5,6 @@ from glob import glob
 IMAGE_EXTENSIONS = {'bmp', 'jpg', 'jpeg', 'pgm', 'png', 'ppm',
                     'tif', 'tiff', 'webp'}
 
-def adjust_range(input, in_range=[0,1], out_range=[-1,1]):
-    '''
-    adjust the dynamic range, from input range to the target range
-    '''
-    if in_range!=out_range:
-        scale = (np.float32(out_range[1])-np.float32(out_range[0]))/(
-            np.float32(in_range[1]-np.float32(in_range[0])))
-        bias = np.float32(out_range[0])-np.float32(in_range[0])*scale
-        out = input*scale+bias
-    return torch.clamp(out, min=out_range[0], max=out_range[1])
-
 def grab_image_path(data_path):
     from functools import reduce
     assert os.path.exists(data_path), 'Image directory does not exist'
